d8baebcd336fc39df80e25f9f92fb7a2272ad3b1
Hamid Sarfaraz <hamidsarfraz8@gmail.com>
2023-07-13 17:14:24 +0530

    Initial Commit

---
+++ src/main/java/com/example/BootLearning/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.BootLearning.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String HelloWorld(){
+        return ("Welcome to hamid learning");
+    }
+
+}
