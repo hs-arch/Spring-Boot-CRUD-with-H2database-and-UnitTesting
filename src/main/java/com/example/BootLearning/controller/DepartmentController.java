910993d71ae16f223d9fb5758d441c7a21eabc30
Hamid Sarfaraz <hamidsarfraz8@gmail.com>
2023-07-23 13:55:58 +0530

    added loggers for each endpoint.

--- src/main/java/com/example/BootLearning/controller/DepartmentController.java
+++ src/main/java/com/example/BootLearning/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package com.example.BootLearning.controller;
 import com.example.BootLearning.entity.Department;
 import com.example.BootLearning.service.DepartmentService;
 import com.example.BootLearning.service.DepartmentServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,28 +19,34 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     //to save the department
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
 
 //        DepartmentService service = new DepartmentServiceImpl();
+        LOGGER.info("inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/fetchDepartments")
     public List<Department> fetchDepartments(){
+        LOGGER.info("inside fetchDepartments of DepartmentController");
         return departmentService.fetchDepartment();
 
     }
 
     @GetMapping("/fetchDepartments/{id}")
     public Department fetchDepartmentsById(@PathVariable("id") Long id){
+        LOGGER.info("inside fetchDepartmentsById of DepartmentController");
         return departmentService.fetchDepartmentById(id);
 
     }
 
     @DeleteMapping("/deleteDepartments/{id}")
     public String deleteDepartmentsById(@PathVariable("id") Long id){
+        LOGGER.info("inside deleteDepartmentsById of DepartmentController");
         departmentService.deleteDepartmentById(id);
 
         return "Record Deleted";
@@ -46,6 +54,7 @@ public class DepartmentController {
 
     @PostMapping("/updateDepartments/{id}")
     public Department updateDepartment(@PathVariable("id") Long id,  Department department){
+        LOGGER.info("inside updateDepartment of DepartmentController");
          return departmentService.updateDepartment(id,department);
         //return "record Updated";
     }
